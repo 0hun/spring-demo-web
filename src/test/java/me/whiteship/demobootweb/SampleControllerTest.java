6ed3a44512ba641b14b2e7400b57fdf4eb85560c
joyounghun <whdudgns2654@gmail.com>
2020-05-16 02:03:49 +0900

    WebMvcConfigure Formatter 프로젝트 생성

---
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -0,0 +1,25 @@
+package me.whiteship.demobootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        this.mockMvc.perform(get("/hello/1"))
+                .andDo(print())
+                .andExpect(content().string("hello 1"));
+    }
+
+}
