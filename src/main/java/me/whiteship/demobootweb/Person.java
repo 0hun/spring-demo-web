52f7a39c44f63dbf8589c30b80e37abf02c105fe
joyounghun <whdudgns2654@gmail.com>
2020-05-16 16:25:18 +0900

    객체를 조회할때는 중복될수있는 name 대신에 id로 조회해야한다. id로 조회하려면 spring-data-jpa가 제공하는 도메인 클래스 컨버터를 이용해야한다.
    id를 이용해 해당 엔티티를 읽어서 person객체에 formatting을 해준다.

--- src/main/java/me/whiteship/demobootweb/Person.java
+++ src/main/java/me/whiteship/demobootweb/Person.java
@@ -1,8 +1,24 @@
 package me.whiteship.demobootweb;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
 
+    @Id @GeneratedValue
+    private Long id;
+
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -10,12 +26,5 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
