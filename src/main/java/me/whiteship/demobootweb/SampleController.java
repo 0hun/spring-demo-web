cda5b944c251e5d7e809488f224fccfd374f5fb0
joyounghun <whdudgns2654@gmail.com>
2020-05-18 13:23:35 +0900

    핸들러 인터셉터 구현 핸들러를 이용하거나 핸들에 종속적인 처리를 할때에는 필터보다 인터셉터 필터는 일반적인 용도로 구현
    WebConfig에 addInterceptor에 핸들러 인터셉터를 등록하여 구현

--- src/main/java/me/whiteship/demobootweb/SampleController.java
+++ src/main/java/me/whiteship/demobootweb/SampleController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-
+    // preHandle 1
+    // preHandle 2
+    // 요청처리
+    // postHandler 2
+    // postHandler 1
+    // afterCompletion 2
+    // afterCompletion 1
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
