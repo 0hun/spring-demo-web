b6072ee07fedcd23f3b622e66194a1910b6efa6e
joyounghun <whdudgns2654@gmail.com>
2020-05-18 14:36:29 +0900

    서블릿 컨테이너가 제공하는 기본 리소스 핸들러 외에 기타 설정 및 다른 리소스 핸들러 설정

--- src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
+++ src/test/java/me/whiteship/demobootweb/SampleControllerTest.java
@@ -1,14 +1,18 @@
 package me.whiteship.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.regex.Matcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,6 +34,14 @@ public class SampleControllerTest {
                     .param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello keesun"));
+    }
+
+    @Test void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 
 }
